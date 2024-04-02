4d22bb9b8cd00b05187ddafd05091d6ba9437d89
Akeops <lalanne.andoni1@gmail.com>
2024-04-02 12:22:55 +0200

    J'ai ajouté le fait de pouvoir mettre une tâche dans la liste grâce au formulaire.

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 const ul = document.querySelector("ul");
-const task = document.querySelector(".task").textContent;
+const li = document.createElement("li");
+const inputTask = document.querySelector(".task");
+let task = null;
 
-function addTask(inputTask) {}
+const listeTache = [];
+
+function addTask(task) {
+  li.textContent = task;
+  ul.append(li);
+  console.log(task);
+}
+
+function getTasks() {
+  const taskList = localStorage.getItem("listeTache");
+}
+
+document.querySelector("form").onsubmit = function (e) {
+  e.preventDefault();
+
+  task = document.querySelector(".form").elements["task"].value;
+  addTask(task);
+  this.reset();
+};
