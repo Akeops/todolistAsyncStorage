a411ffc97943ea88e4ac1b0a07779acc00c09df8
Akeops <lalanne.andoni1@gmail.com>
2024-04-03 21:57:33 +0200

    J'ai mis en place la liste des tâches depuis le LocalStorage.

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
+let li;
 const ul = document.querySelector("ul");
-const li = document.createElement("li");
 const resetButton = document.querySelector(".resetButton");
-const inputTask = document.querySelector(".task");
-let task = null;
+const inputTask = document.querySelector(".inputTask");
 let taskList = [];
 
+// Fonction qui ajoute une tâche.
 function addTaskToLocalStorage(task) {
-  taskList.push(task);
-  localStorage.setItem("taskList", JSON.stringify(taskList));
-  li.textContent = task;
-  ul.append(li);
+  try {
+    taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+    if (!Array.isArray(taskList)) {
+      taskList = [];
+    } else {
+      taskList.push(task);
+      localStorage.setItem("taskList", JSON.stringify(taskList));
+      getTasks();
+    }
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-function getTasks() {
-  taskList = JSON.parse(localStorage.getItem("taskList"));
-  taskList.forEach((task) => {
-    li.textContent = task;
-    ul.append(li);
-  });
-}
-
-function handleResetButton() {
-  localStorage.clear();
-}
-
+// Récupère les données du formulaire et execute la fonction d'ajout de tâche.
 document.querySelector("form").onsubmit = function (e) {
   e.preventDefault();
 
-  task = document.querySelector(".form").elements["task"].value;
+  let task = document.querySelector(".form").elements["task"].value;
   addTaskToLocalStorage(task);
   this.reset();
 };
 
-resetButton.addEventListener("click", handleResetButton);
+function getTasks() {
+  ul.textContent = "";
+  taskList = JSON.parse(localStorage.getItem("taskList"));
+  taskList.forEach((task) => {
+    addTaskToDom(task);
+  });
+}
+
+function addTaskToDom(task) {
+  const li = document.createElement("li");
+  li.textContent = task;
+  ul.appendChild(li);
+}
+
+function handleResetButton() {
+  localStorage.removeItem("taskList");
+}
+
+getTasks();
+resetButton.addEventListener("click", () => {
+  handleResetButton();
+  getTasks();
+});
